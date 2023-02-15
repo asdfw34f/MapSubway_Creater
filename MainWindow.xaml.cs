03b6e6e0c5d42c1ed35b873b6f0d2c151f8af5c6
Daniil Demekhin <daniil2002.d11@gmail.com>
2023-02-16 00:26:31 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string path = "pt"; const string st = "el"; const string arr = "cr";
+        string index = arr;
+        bool paint = false;
+        Point px, py = new Point();
+        Pen pen = new Pen(Brushes.Black, 5);
+        DrawingBrush brush = new DrawingBrush();
+        Geometry geometry;
+        PathFigure currentFigure;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPath_Click(object sender, RoutedEventArgs e)
+        {
+            index = path;
+        }
+
+        private void btnStation_Click(object sender, RoutedEventArgs e)
+        {
+            index = st;
+
+        }
+
+        private void btnCursor_Click(object sender, RoutedEventArgs e)
+        {
+            index = arr;
+            //GeometryDrawingExample();
+        }
+
+
+        void StartFigure(Point start)
+        {
+            currentFigure = new PathFigure() { StartPoint = start };
+            var currentPath =
+                new Path()
+                {
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 3,
+                    Data = new PathGeometry() { Figures = { currentFigure } }
+                };
+            canDrawing.Children.Add(currentPath);
+        }
+        void AddFigurePoint(Point point)
+        {
+            currentFigure.Segments.Add(new LineSegment(point, isStroked: true));
+        }
+        void EndFigure()
+        {
+            currentFigure = null;
+        }
+
+
+        private void canDrawing_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!paint)
+                return;
+            AddFigurePoint(e.GetPosition(canDrawing));
+
+        }
+
+        private void canDrawing_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            py = e.GetPosition(this);
+            AddFigurePoint(e.GetPosition(canDrawing));
+            EndFigure();
+            Mouse.Capture(null);
+            paint = false;
+        }
+
+        private void canDrawing_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            px = e.GetPosition(this);
+            Mouse.Capture(canDrawing);
+            StartFigure(e.GetPosition(canDrawing));
+        }
+    }
+}
---
+++ Model/ControlUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace EditorSubwayMap.Model
+{
+    public class ControlUser : MouseEventArgs
+    {
+        public ControlUser(MouseDevice mouse, int timestamp) : base(mouse, timestamp)
+        {
+            if (mouse != null)
+            {
+
+            }
+        }
+        public void ads(MouseEventArgs e, Canvas pic, Point px, Point py)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Pen pen = new Pen(Brushes.Black, 5);
+                DrawingBrush brush = new DrawingBrush();
+                Geometry geometry;
+                geometry = new LineGeometry(px, py);
+                Line line = new Line();
+                GeometryDrawing geometryDrawing = new GeometryDrawing(brush, pen, geometry);
+                geometryDrawing.Geometry = geometry;
+                DrawingImage image = new DrawingImage();
+                image.Drawing = geometryDrawing;
+            }
+
+        }
+    }
+
+}
