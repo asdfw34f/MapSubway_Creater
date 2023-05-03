5d40ddd258ff3f0ceaafd31e1a893208c99c9f8e
asdfw34f <daniil2002.d11@gmail.com>
2023-05-03 10:46:59 +0300

    temp

---
+++ Models/CanvasModel.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace EditorSubwayMap.Models
+{
+    public class CanvasModel { }
+
+    public class MyCanvas : INotifyPropertyChanged
+    {
+        #region Fields
+        private Canvas _canvas;
+        private bool _isDrawing;
+        private Point _startPoint;
+        private List<UIElement> _Children;
+        private string _DrawMode;
+        #endregion
+
+        #region Propertyes
+        public string DrawMode
+        {
+            get => _DrawMode;
+            set
+            {
+                if (value == "Line" || value == "Circle" || value == "Station" || value == "")
+                {
+                    _DrawMode = value;
+                    RaisePropertyChanged(nameof(DrawMode));
+                }
+            }
+        }
+
+        public List<UIElement> Children
+        {
+            get => _Children;
+            set
+            {
+                _Children = value;
+                RaisePropertyChanged(nameof(Children));
+            }
+        }
+
+        public Point startPoint
+        {
+            get => _startPoint;
+            set
+            {
+                _startPoint = value;
+                RaisePropertyChanged("startPoint");
+            }
+        }
+
+        public bool isDrawing
+        {
+            get => _isDrawing;
+            set
+            {
+                _isDrawing = value;
+                RaisePropertyChanged(nameof(isDrawing));
+            }
+        }
+
+        public Canvas canvas
+        {
+            get => _canvas;
+            set
+            {
+                if (_canvas != value)
+                {
+                    _canvas = value;
+                    RaisePropertyChanged("canvas");
+                }
+            }
+        }
+
+        #endregion 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ View/CanvasView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using EditorSubwayMap.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EditorSubwayMap.View
+{
+    /// <summary>
+    /// Логика взаимодействия для CanvasView.xaml
+    /// </summary>
+    public partial class CanvasView : UserControl
+    {
+        public CanvasView()
+        {
+            InitializeComponent();
+            DataContext = new CanvasViewModel(new MainViewModel());
+        }
+    }
+}
